10877e67b735765f492c667bbebdb5d5607295d2
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2023-06-29 23:31:56 +0530

    added more authentication and  user name in posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postsControllers = require('../controllers/posts_controller');
 
-router.post('/create', postsControllers.create);
+
+router.post('/create',passport.checkAuthentication, postsControllers.create);
 
 
 
